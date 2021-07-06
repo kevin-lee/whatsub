ca122880ebbbadad9fb3f310542790980d524023
Kevin Lee <kevin.code@kevinlee.io>
2021-07-07 00:13:15 +1000

    Close #32 - Remove redundant code in Whatsub

--- cli/src/main/scala/whatsub/Whatsub.scala
+++ cli/src/main/scala/whatsub/Whatsub.scala
@@ -16,6 +16,38 @@ import scala.io.Source
   */
 object Whatsub {
 
+  private def parseAndConvert[F[_]: Sync, A, B: CanRender](
+    parser: String => Either[ParseError, A],
+    src: File,
+    outFile: Option[ConvertArgs.OutFile],
+  )(
+    using convert: Convert[A, B],
+  ): F[Either[WhatsubError, Unit]] =
+    (for {
+      srcSub <- EitherT(
+                  Resource
+                    .make(Sync[F].delay(Source.fromFile(src)))(source => Sync[F].delay(source.close()))
+                    .use { srcSource =>
+                      Sync[F]
+                        .delay(srcSource.getLines.map(_.trim).mkString("\n"))
+                        .flatMap(lines => Sync[F].delay(parser(lines)))
+                    },
+                ).leftMap(WhatsubError.ParseFailure(_))
+      outSub <- EitherT(Sync[F].delay(convert.convert(srcSub)))
+                  .leftMap(WhatsubError.ConversionFailure(_))
+      _      <- EitherT
+                  .right(Sync[F].delay(outFile))
+                  .flatMapF(
+                    _.fold(
+                      Sync[F].delay(println(CanRender[B].render(outSub)).asRight),
+                    )(out => FileF.fileF[F].writeFile(outSub, out.outFile)),
+                  )
+                  .leftMap {
+                    case FileError.WriteFilure(file, throwable) =>
+                      WhatsubError.FileWriteFailure(file, throwable)
+                  }
+    } yield ()).value
+
   def apply[F[_]: Sync](args: WhatsubArgs): F[Either[WhatsubError, Unit]] = args match {
     case ConvertArgs(
           ConvertArgs.From(SupportedSub.Smi),
@@ -24,34 +56,7 @@ object Whatsub {
           outFile,
         ) =>
       val src = srcFile.srcFile.getCanonicalFile
-      (for {
-        smi <- EitherT(
-                 Resource
-                   .make(Sync[F].delay(Source.fromFile(src)))(source => Sync[F].delay(source.close()))
-                   .use { srcSource =>
-                     Sync[F]
-                       .delay(srcSource.getLines.map(_.trim).mkString("\n"))
-                       .flatMap(lines => Sync[F].delay(SmiParser.parse(lines)))
-                   },
-               ).leftMap(WhatsubError.ParseFailure(_))
-        srt <- EitherT(Sync[F].delay(Convert[Smi, Srt].convert(smi)))
-                 .leftMap(WhatsubError.ConversionFailure(_))
-        _   <- EitherT
-                 .right(Sync[F].pure(outFile))
-                 .flatMapF(
-                   _.fold(
-                     Sync[F].delay(println(srt.render).asRight),
-                   )(out =>
-                     FileF
-                       .fileF[F]
-                       .writeFile(srt, out.outFile),
-                   ),
-                 )
-                 .leftMap {
-                   case FileError.WriteFilure(file, throwable) =>
-                     WhatsubError.FileWriteFailure(file, throwable)
-                 }
-      } yield ()).value
+      parseAndConvert[F, Smi, Srt](SmiParser.parse, src, outFile)
 
     case ConvertArgs(
           ConvertArgs.From(SupportedSub.Srt),
@@ -60,35 +65,7 @@ object Whatsub {
           outFile,
         ) =>
       val src = srcFile.srcFile.getCanonicalFile
-      (for {
-        srt <- EitherT(
-                 Resource
-                   .make(Sync[F].delay(Source.fromFile(src)))(source => Sync[F].delay(source.close()))
-                   .use { srcSource =>
-                     Sync[F]
-                       .delay(srcSource.getLines.map(_.trim).mkString("\n"))
-                       .flatMap(lines => Sync[F].delay(SrtParser.parse(lines)))
-                   },
-               ).leftMap(WhatsubError.ParseFailure(_))
-        smi <- EitherT(Sync[F].delay(Convert[Srt, Smi].convert(srt)))
-                 .leftMap(WhatsubError.ConversionFailure(_))
-        _   <-
-          EitherT
-            .right(Sync[F].pure(outFile))
-            .flatMapF(
-              _.fold(
-                Sync[F].delay(println(smi.render).asRight),
-              )(out =>
-                FileF
-                  .fileF[F]
-                  .writeFile(smi, out.outFile),
-              ),
-            )
-            .leftMap {
-              case FileError.WriteFilure(file, throwable) =>
-                WhatsubError.FileWriteFailure(file, throwable)
-            }
-      } yield ()).value
+      parseAndConvert[F, Srt, Smi](SrtParser.parse, src, outFile)
 
     case ConvertArgs(ConvertArgs.From(SupportedSub.Smi), ConvertArgs.To(SupportedSub.Smi), _, _) =>
       Sync[F].pure(WhatsubError.NoConversion(SupportedSub.Smi).asLeft)
