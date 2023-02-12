8247b89f55efbdeef44c92a6dd613683dd42ef33
Kevin Lee <kevin.code@kevinlee.io>
2023-02-12 20:09:59 +1100

    Clean up and rename subtitle file for testing

--- modules/whatsub-core/src/test/scala/whatsub/parse/SrtParserSpec.scala
+++ modules/whatsub-core/src/test/scala/whatsub/parse/SrtParserSpec.scala
@@ -1,22 +1,11 @@
 package whatsub.parse
 
-import cats.Monad
 import cats.effect.IO
-import cats.syntax.all.*
-import effectie.core.*
 import effectie.instances.ce3.fx.given
-import effectie.resource.{Ce3ResourceMaker, ResourceMaker}
-import effectie.syntax.all.*
-import extras.cats.syntax.all.*
-import extras.hedgehog.ce3.CatsEffectRunner
 import hedgehog.*
 import hedgehog.runner.*
 import whatsub.parse.SubParsers.*
-import whatsub.{CanRender, FileF, GoldenTest, Smi, Srt}
-
-import java.io.File
-import scala.io.{Codec, Source}
-import scala.reflect.{ClassTag, classTag}
+import whatsub.*
 
 /** @author Kevin Lee
   * @since 2023-02-12
@@ -25,8 +14,8 @@ object SrtParserSpec extends Properties {
   override def tests: List[Test] = List(
     ParserGoldenTester.goldenTestParse[Srt]("golden/test-src.srt", "golden/test-src.srt", SrtParser.parse[IO]),
     ParserGoldenTester.goldenTestParse[Srt](
-      "golden/test-src-with-empty-lines.srt",
-      "golden/test-src-with-empty-lines.srt",
+      "golden/test-srt-parse-with-empty-lines.srt",
+      "golden/test-srt-parse-with-empty-lines.srt",
       SrtParser.parse[IO]
     ),
   )
